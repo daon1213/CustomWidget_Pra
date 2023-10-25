47c5f5aee78cf8f1b0058e88fa8fba1632506a37
daon1213 <diaeiyo1213@naver.com>
2023-10-25 20:27:39 +0900

    💡Viewbinding 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.daon.customwidget_pra"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.daon.customwidget_pra"
